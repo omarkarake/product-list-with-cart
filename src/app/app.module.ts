83eca18c7daa5df545064f2019352a440a338ea5
omar karake <omar.karake@amalitechtraining.org>
2024-08-20 20:27:55 +0200

    ft: finalizing the modal for the oderconfirmation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,26 +1,20 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
+import {
+  BrowserModule,
+  provideClientHydration,
+} from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CardComponent } from './components/card/card.component';
 import { provideHttpClient, withFetch } from '@angular/common/http';
 import { OrderconfirmModalComponent } from './components/orderconfirm-modal/orderconfirm-modal.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    CardComponent,
-    OrderconfirmModalComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [
-    provideHttpClient(withFetch()),
-    provideClientHydration()
-  ],
-  bootstrap: [AppComponent]
+  declarations: [AppComponent, CardComponent, OrderconfirmModalComponent],
+  imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule],
+  providers: [provideHttpClient(withFetch()), provideClientHydration()],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/components/orderconfirm-modal/orderconfirm-modal.component.ts
+++ src/app/components/orderconfirm-modal/orderconfirm-modal.component.ts
@@ -1,9 +1,24 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
+import {
+  trigger,
+  state,
+  style,
+  transition,
+  animate,
+} from '@angular/animations';
 
 @Component({
   selector: 'app-orderconfirm-modal',
   templateUrl: './orderconfirm-modal.component.html',
   styleUrl: './orderconfirm-modal.component.css',
+  animations: [
+    trigger('slideUpDown', [
+      state('void', style({ transform: 'translateY(100%)', opacity: 0 })),
+      state('*', style({ transform: 'translateY(0)', opacity: 1 })),
+      transition('void => *', [animate('300ms ease-out')]),
+      transition('* => void', [animate('300ms ease-in')]),
+    ]),
+  ],
 })
 export class OrderconfirmModalComponent {
   @Input() isVisible: boolean = false;
