d75905f9c952c693392b4223a870cac20abdf83d
omar karake <omar.karake@amalitechtraining.org>
2024-08-20 19:08:54 +0200

    chore: making layout and rendering the cards

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,47 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, OnInit } from '@angular/core';
 import { StoreService } from './store/store.service';
+import { Subscription } from 'rxjs';
+import { Dessert } from './models/image.model';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
-export class AppComponent implements OnInit {
+export class AppComponent implements OnInit, AfterViewInit {
   title = 'productListWithCart';
+  private subscriptions: Subscription = new Subscription();
+  desserts: Dessert[] = [];
+  incrementState: boolean = false;
+  quantityState: number = 0;
+
   constructor(private store: StoreService) {}
   ngOnInit(): void {
     this.store.init();
+  }
+
+  ngAfterViewInit(): void {
+    this.subscriptions.add(
+      this.store.desserts$.subscribe((data) => {
+        this.desserts = data;
+      })
+    );
+    console.log('desserts in app: ', this.desserts);
+  }
+
+  ngOnDestroy(): void {
+    this.subscriptions.unsubscribe();
+  }
+
+  handleAddToCart(dessert: any) {
+    // Handle add to cart logic
+  }
+
+  handleIncrementQuantity(dessert: any) {
+    // Handle increment quantity logic
+  }
+
+  handleDecrementQuantity(dessert: any) {
+    // Handle decrement quantity logic
   }
 }
--- src/app/components/card/card.component.ts
+++ src/app/components/card/card.component.ts
@@ -1,37 +1,82 @@
-import { Component, OnInit } from '@angular/core';
-import { StoreService } from '../../store/store.service';
-import { Subscription } from 'rxjs';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { Dessert } from '../../models/image.model';
 
 @Component({
   selector: 'app-card',
   templateUrl: './card.component.html',
-  styleUrl: './card.component.css',
+  styleUrls: ['./card.component.css'],
 })
-export class CardComponent implements OnInit {
-  private subscriptions: Subscription = new Subscription();
-  desserts: Dessert[] = [];
-  singleDessert: Dessert | undefined;
-  incrementState: boolean = false;
+export class CardComponent {
+  @Input() singleDessert: Dessert | undefined;
+  @Input() incrementState: boolean = false;
+  @Input() quantityState: number = 0;
 
-  constructor(private store: StoreService) {}
-
-  ngOnInit(): void {
-    this.subscriptions.add(
-      this.store.desserts$.subscribe((data) => {
-        this.desserts = data;
-        this.singleDessert = this.desserts[0];
-        console.log('data: ', this.singleDessert);
-      })
-    );
-  }
-
-  ngOnDestroy(): void {
-    this.subscriptions.unsubscribe();
-  }
+  @Output() addToCart = new EventEmitter<void>();
+  @Output() incrementQuantity = new EventEmitter<void>();
+  @Output() decrementQuantity = new EventEmitter<void>();
 
   addToCartActive() {
-    this.incrementState = true;
-    console.log('incrementState: ', this.incrementState);
+    this.addToCart.emit();
+  }
+
+  incrementQuantityHandler(event: Event) {
+    event.stopPropagation();
+    this.incrementQuantity.emit();
+  }
+
+  decrementQuantityHandler(event: Event) {
+    event.stopPropagation();
+    this.decrementQuantity.emit();
   }
 }
+
+// import { Component, OnInit } from '@angular/core';
+// import { StoreService } from '../../store/store.service';
+// import { Subscription } from 'rxjs';
+// import { Dessert } from '../../models/image.model';
+
+// @Component({
+//   selector: 'app-card',
+//   templateUrl: './card.component.html',
+//   styleUrl: './card.component.css',
+// })
+// export class CardComponent implements OnInit {
+//   private subscriptions: Subscription = new Subscription();
+//   desserts: Dessert[] = [];
+//   singleDessert: Dessert | undefined;
+//   incrementState: boolean = false;
+//   quantityState: number = 0;
+
+//   constructor(private store: StoreService) {}
+
+//   ngOnInit(): void {
+//     this.subscriptions.add(
+//       this.store.desserts$.subscribe((data) => {
+//         this.desserts = data;
+//         this.singleDessert = this.desserts[0];
+//         console.log('data: ', this.singleDessert);
+//       })
+//     );
+//   }
+
+//   ngOnDestroy(): void {
+//     this.subscriptions.unsubscribe();
+//   }
+
+//   addToCartActive() {
+//     this.incrementState = true;
+//     console.log('incrementState: ', this.incrementState);
+//   }
+
+//   incrementQuantity(event: any) {
+//     event.stopPropagation();
+//     this.quantityState++;
+//     console.log('quantity state: ', this.quantityState);
+//   }
+
+//   decrementQuantity(event: any) {
+//     event.stopPropagation();
+//     this.quantityState--;
+//     console.log('quantity state: ', this.quantityState);
+//   }
+// }
